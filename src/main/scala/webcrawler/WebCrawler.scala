ea7078f27c81aa7bec804ca624c57b3ce9d90f7b
jlegvart <jernej.legvart@protonmail.com>
2021-11-10 21:35:41 +0100

    WebCrawler - parse html content & save links to queue

--- src/main/scala/webcrawler/WebCrawler.scala
+++ src/main/scala/webcrawler/WebCrawler.scala
@@ -15,6 +15,13 @@ import org.http4s.implicits._
 import org.http4s.server.blaze._
 import java.util.regex.Pattern
 import java.util.regex.Matcher
+import org.jsoup.Jsoup
+import collection.JavaConverters._
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+import cats.syntax.all._
+import cats.effect.syntax.all._
 
 object WebCrawler {
 
@@ -25,32 +32,61 @@ object WebCrawler {
 
 class WebCrawler[F[_]: Async: Console](client: Client[F]) {
 
-  val pattern = "href=\"(\\/wiki\\/.*?)\"".r;
-  val wiki = ""
-
   def start(uri: Uri) =
     for {
-      urlQ <- Queue.unbounded[F, String]
+      urlQ <- Queue.unbounded[F, Uri]
       resQ <- Queue.unbounded[F, WebCrawler.CrawlerResult]
 
       _ <- Console[F].println(s"Starting scraping: ${uri.toString()}")
-      - <- urlQ.offer(uri.toString())
+      - <- urlQ.offer(uri)
       _ <- crawl(urlQ, resQ)
     } yield ()
 
-  def crawl(urlQueue: Queue[F, String], results: Queue[F, WebCrawler.CrawlerResult]): F[Unit] =
+  def crawl(urlQueue: Queue[F, Uri], results: Queue[F, WebCrawler.CrawlerResult]): F[Unit] =
     for {
       next <- urlQueue.take
-      _ <- Console[F].println(s"Crawling next url: $next")
+      _ <- Console[F].println(s"Crawling next url: ${next.toString()}")
 
-      result <- client.expect[String](next)
-      a =
-        pattern
-          .findAllMatchIn(result)
-          .map(_.group(1))
-          .toSeq
+      host <- getFullHost(next)
+      html <- client.expect[String](next)
+      htmlDocument = Jsoup.parse(html)
+      title <- title(htmlDocument)
+      content <- parseContent(htmlDocument)
+      links <- parseLinks(host, htmlDocument)
 
-      _ <- Console[F].println(a)
+      _ <- links.traverse(urlQueue.offer(_))
     } yield ()
+
+  def getFullHost(url: Uri) =
+    (url.host, url.scheme) match {
+      case (Some(host), Some(scheme)) =>
+        Async[F].pure(scheme.value.toString().concat("://").concat(host.toString()))
+      case _ =>
+        Async[F].raiseError(
+          new RuntimeException(s"Invalid url, cannot extract scheme & host from: $url")
+        )
+    }
+
+  def title(html: Document): F[String] = Async[F].pure(html.title())
+
+  def parseLinks(host: String, html: Document): F[Seq[Uri]] = Async[F].pure(
+    html
+      .select("#bodyContent a[href*=\"/wiki/\"]")
+      .eachAttr("href")
+      .asScala
+      .toSeq
+      .filter(!_.contains(":"))
+      .map {
+        case x if x.startsWith("http") => Some(x)
+        case x if x.startsWith("/")    => Some(host.concat(x))
+        case _                         => None
+      }
+      .flatten
+      .map(Uri.fromString(_).toOption)
+      .flatten
+      .distinct
+  )
+
+  def parseContent(html: Document): F[String] = Async[F].pure(html.text())
 
 }
