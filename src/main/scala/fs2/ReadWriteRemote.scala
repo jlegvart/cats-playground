23b948f284a16b368c8b16b65f0e3336af48a860
jlegvart <jernej.legvart@protonmail.com>
2021-11-21 15:50:26 +0100

    fs2 - readwrite read remote stream

---
+++ src/main/scala/fs2/ReadWriteRemote.scala
@@ -0,0 +1,51 @@
+package fs2
+
+import cats.effect.IOApp
+import org.http4s.client.blaze._
+import org.http4s.client._
+import scala.concurrent.ExecutionContext.global
+import cats.effect.IO
+import cats._
+import cats.effect._
+import cats.implicits._
+import org.http4s.Uri
+import org.http4s.client.dsl.io._
+import org.http4s.headers._
+import org.http4s.MediaType
+import org.http4s.Method._
+import org.http4s.Request
+import org.http4s.Method
+import cats.data.EitherT
+import scala.concurrent.duration._
+
+object ReadWriteRemote extends IOApp.Simple {
+
+  val endpoint = "https://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt"
+
+  def run =
+    (for {
+      u <- Uri.fromString(endpoint)
+      stream <- getRemoteResource(u)
+    } yield stream)
+      .fold(parseError => IO.raiseError(new RuntimeException(parseError.message)), _.compile.drain)
+
+  def getRemoteResource(uri: Uri) = {
+    val a =
+      for {
+        client <- initClient
+        req = Request[IO](Method.GET, uri)
+        chunk <- client.stream(req).flatMap(_.body.chunks)
+        _ <- processChunk(chunk)
+      } yield ()
+    Right(a)
+  }
+
+  def initClient() = BlazeClientBuilder[IO](global).stream
+
+  def processChunk(chunk: Chunk[Byte]) = Stream
+    .chunk(chunk)
+    .through(text.utf8.decode)
+    .through(text.lines)
+    .map(println(_))
+
+}
